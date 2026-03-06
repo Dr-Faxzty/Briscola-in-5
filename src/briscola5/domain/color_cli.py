725726af87e6f41eb15e08b16d6004cec19f2eae
Francesco <rfrancescopietro04@gmail.com>
2026-03-06 17:58:32 +0100

    Add reset auction and updated GameService

---
+++ src/briscola5/domain/color_cli.py
@@ -0,0 +1,9 @@
+class Col:
+    CYAN = "\033[96m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    RED = "\033[91m"
+    MAGENTA = "\033[95m"
+    BLUE = "\033[94m"
+    BOLD = "\033[1m"
+    RESET = "\033[0m"
